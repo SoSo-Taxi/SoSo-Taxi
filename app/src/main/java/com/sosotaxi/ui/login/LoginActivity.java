12551fa234653e82bd4a1e0feb2c7923b5b58c70
leaf1170124460 <45704618+leaf1170124460@users.noreply.github.com>
2020-07-09 15:02:52 +0800

    F-更改主题色，建立初步应用架构

---
+++ app/src/main/java/com/sosotaxi/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.sosotaxi.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sosotaxi.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
