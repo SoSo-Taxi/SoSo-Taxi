5a15bc2ab7ae23f89865b7562f1468149e90e91a
Clement <48651760+YueClementine@users.noreply.github.com>
2020-07-10 16:29:49 +0800

    添加定位功能

--- app/src/main/java/com/sosotaxi/ui/home/HomeFragment.java
+++ app/src/main/java/com/sosotaxi/ui/home/HomeFragment.java
@@ -11,15 +11,46 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.CoordType;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 import com.sosotaxi.R;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener{
 
     private HomeViewModel mViewModel;
 
     private MapView mMapView = null;
+    private BaiduMap mBaiduMap = null;
+    private HomeFragment context;
+    //LOCATION
+    private double mLatitude;
+    private double mLongtitude;
+    //SENSOR
+    private MyOrientationListener mMyOrientationListener;
+    private float mCurrentX;
+
+    //ICON
+    private BitmapDescriptor mIconLocation;
+    private LocationClient mLocationClient;
+    public BDAbstractLocationListener myListener;
+    private LatLng mLastLocationData;
+    private boolean isFirstin = true;
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
@@ -29,17 +60,45 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-
         //获取地图控件引用
         mMapView = (MapView)root.findViewById(R.id.baiduMapView);
+        mBaiduMap = mMapView.getMap();
         return root;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        SDKInitializer.initialize(getContext());
+        SDKInitializer.setCoordType(CoordType.BD09LL);
+        this.context = this;
+        initMyLocation();
+        //按钮
+        Button locationButton = (Button) getActivity().findViewById(R.id.but_Loc);
+        //按钮处理
+        locationButton.setOnClickListener(this);
+
         mViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         // TODO: Use the ViewModel
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        //开启定位
+        mBaiduMap.setMyLocationEnabled(true);
+        if (!mLocationClient.isStarted())
+            mLocationClient.start();
+        //开启方向传感器
+        mMyOrientationListener.start();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        //停止定位
+        mBaiduMap.setMyLocationEnabled(false);
+        mLocationClient.stop();
+        //停止方向传感器
+        mMyOrientationListener.stop();
     }
 
     @Override
@@ -57,8 +116,115 @@ public class HomeFragment extends Fragment {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        mBaiduMap.setMyLocationEnabled(false);
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mMapView.onDestroy();
+        mMapView = null;
     }
+    @Override
+    public void onClick(View v) {
+        SDKInitializer.initialize(getContext());
+        switch (v.getId()) {
+            case R.id.but_Loc: {
+                centerToMyLocation(mLatitude, mLongtitude);
+                break;
+            }
+        }
+    }
+
+    //定位
+    private class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null){
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(mCurrentX).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            //设置自定义图标
+            MyLocationConfiguration config = new
+                    MyLocationConfiguration(
+                    MyLocationConfiguration.LocationMode.NORMAL, true, mIconLocation);
+            mBaiduMap.setMyLocationConfiguration(config);
+            //更新经纬度
+            mLatitude = location.getLatitude();
+            mLongtitude = location.getLongitude();
+            //设置起点
+            mLastLocationData = new LatLng(mLatitude, mLongtitude);
+            if (isFirstin) {
+                centerToMyLocation(location.getLatitude(), location.getLongitude());
+
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {
+                    // GPS定位结果
+                    Toast.makeText(getActivity(), "定位:"+location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                    // 网络定位结果
+                    Toast.makeText(getActivity(), "定位:"+location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {
+                    // 离线定位结果
+                    Toast.makeText(getActivity(), "定位:"+location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    Toast.makeText(getActivity(), "定位:服务器错误", Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    Toast.makeText(getActivity(), "定位:网络错误", Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    Toast.makeText(getActivity(), "定位:手机模式错误，请检查是否飞行", Toast.LENGTH_SHORT).show();
+                }
+                isFirstin = false;
+            }
+        }
+    }
+    //初始化定位
+    private void initMyLocation() {
+        //缩放地图
+        MapStatusUpdate msu = MapStatusUpdateFactory.zoomTo(15.0f);
+        mBaiduMap.setMapStatus(msu);
+        //开启定位
+        mBaiduMap.setMyLocationEnabled(true);
+        //声明LocationClient类
+        mLocationClient = new LocationClient(getActivity());
+        //通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setIsNeedAddress(true);//设置是否需要地址信息
+        option.setScanSpan(1000);
+        //设置locationClientOption
+        mLocationClient.setLocOption(option);
+        myListener = new MyLocationListener();
+        //注册监听函数
+        mLocationClient.registerLocationListener(myListener);
+        //初始化图标
+        mIconLocation = BitmapDescriptorFactory.fromResource(R.drawable.ic_home_location);
+        initOrientation();
+        //开始定位
+        mLocationClient.start();
+    }
+    //回到定位中心
+    private void centerToMyLocation(double latitude, double longtitude) {
+        mBaiduMap.clear();
+        mLastLocationData = new LatLng(latitude, longtitude);
+        MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(mLastLocationData);
+        mBaiduMap.animateMapStatus(msu);
+    }
+    //传感器
+    private void initOrientation() {
+        //传感器
+        mMyOrientationListener = new MyOrientationListener(getContext());
+        mMyOrientationListener.setOnOrientationListener(new MyOrientationListener.OnOrientationListener() {
+            @Override
+            public void onOrientationChanged(float x) {
+                mCurrentX = x;
+            }
+        });
+    }
+
+
+
 
 }
