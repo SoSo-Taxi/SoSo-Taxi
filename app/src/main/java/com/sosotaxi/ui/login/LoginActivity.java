302f2a82b74024614f0e0729a44d525dfa5a3952
leaf1170124460 <45704618+leaf1170124460@users.noreply.github.com>
2020-07-11 19:29:12 +0800

    Revert "Merge pull request #5 from SoSo-Taxi/F"
    This reverts commit 876dfb971b0fc7a77581451b1d308074158dea91, reversing
    changes made to 3f2208fe45b7f6fab26c997c806eecffaaa45dbc.

--- app/src/main/java/com/sosotaxi/ui/login/LoginActivity.java
+++ app/src/main/java/com/sosotaxi/ui/login/LoginActivity.java
@@ -1,24 +1,10 @@
 package com.sosotaxi.ui.login;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.SearchView;
-import android.widget.Spinner;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
 
 import com.sosotaxi.R;
-import com.sosotaxi.common.Constant;
-import com.sosotaxi.ui.login.EnterPhoneFragment;
-import com.sosotaxi.ui.main.MainActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -26,49 +12,5 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        Toolbar toolbar = findViewById(R.id.toolbarLogin);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.fragmentLogin, new EnterPhoneFragment(), null);
-        fragmentTransaction.commit();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_login, menu);
-
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                getSupportFragmentManager().popBackStack();
-                if (fragmentManager.getBackStackEntryCount() == 1) {
-                    setBackUpButtonOff();
-                }
-                break;
-            case R.id.menu_login_skip:
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void setBackUpButtonOn() {
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    public void setBackUpButtonOff() {
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
     }
 }
