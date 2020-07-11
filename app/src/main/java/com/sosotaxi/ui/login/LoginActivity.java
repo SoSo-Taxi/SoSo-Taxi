59e00e484e27f23a1fc6f654bb6a6c7d5b0b6326
leaf1170124460 <45704618+leaf1170124460@users.noreply.github.com>
2020-07-11 19:32:01 +0800

    F-增补合并丢失部分

--- app/src/main/java/com/sosotaxi/ui/login/LoginActivity.java
+++ app/src/main/java/com/sosotaxi/ui/login/LoginActivity.java
@@ -1,10 +1,32 @@
 package com.sosotaxi.ui.login;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.sosotaxi.R;
+import com.sosotaxi.common.Constant;
+import com.sosotaxi.ui.main.MainActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +34,47 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toolbar toolbar = findViewById(R.id.toolbarLogin);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragmentLogin,new EnterPhoneFragment(),null);
+        fragmentTransaction.commit();
+    }
+
+    public void onSelectAreaCode(View view) {
+        Intent intent = new Intent(LoginActivity.this, SelectAreaCodeActivity.class);
+        startActivityForResult(intent, Constant.SELECT_AREA_CODE_REQUEST);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_login, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_login_skip:
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setBackUpButtonOn(){
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void setBackUpButtonOff(){
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
     }
 }
