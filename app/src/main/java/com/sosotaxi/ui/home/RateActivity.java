4ec4fc9025ba0fe17d787e479c0250ed2bc0f4fe
Clement <48651760+YueClementine@users.noreply.github.com>
2020-07-25 08:07:05 +0800

    Y-add rateact

--- app/src/main/java/com/sosotaxi/ui/home/RateActivity.java
+++ app/src/main/java/com/sosotaxi/ui/home/RateActivity.java
@@ -1,4 +1,38 @@
 package com.sosotaxi.ui.home;
 
-public class RateActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.sosotaxi.R;
+
+
+public class RateActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate);
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+    }
 }
